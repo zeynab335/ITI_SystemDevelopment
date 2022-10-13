e6264c60eedd9924b040fba5b9cd64ce49213bcf
zeynab335 <zeynab335>
2022-10-13 15:39:25 +0200

    add simple code in Day3 Description: Bitwise , Search char Fuction

---
+++ Day3/Day3/CProgramming_DD1/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // Point 1
+    printf("Hello World \n");
+
+    // *************************************
+
+    // Point 2
+    printf("Please Enter Number \n");
+    int num;
+    scanf("%d" , &num );
+
+    // print hexadecimal
+    printf("Hexadecimal is : %x \n",num);
+
+    // print ascii code
+    printf("ascii code : %c \n" , num);
+
+
+
+
+
+    // ***************************************
+
+
+
+}
---
+++ Day3/Day3/DD1_Point2/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // Point 2
+    printf("Please Enter Character \n");
+    char ch;
+
+    scanf("%c", &ch);
+    //print to console
+    printf("Ascii Code is %d\n", ch);
+
+    return 0;
+}
