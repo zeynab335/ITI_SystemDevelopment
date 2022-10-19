56984af1887f0aed50e3c72c97f91ab6fd8d06a1
zeynab335 <zeynab335>
2022-10-19 12:01:31 +0200

    add Demo in Day4

---
+++ CProgramming-Day4/Demo/DD4_1/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main()
+{
+    // Print Ascii of Extended Keys
+    char ch ;
+
+    do{
+        printf("Please Press any Key \n" );
+        ch = _getch();
+
+        /// check if key is Extended Using First Byte == -32 Or Not
+        if(ch == -32){
+            ch=_getch();  // print the second byte from Buffer
+            printf("You Press in Extended Key , And Ascii Code is %i " , ch);
+        }
+        else{
+            printf("You Press in Normal Key %c , And Ascii Code is %i " ,ch ,ch);
+        }
+    }
+    while(ch != 0x0d);
+
+    return 0;
+}
