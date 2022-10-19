23f28bfd1830736ac3420c5d22ff3fce2c6e1593
zeynab335 <zeynab335>
2022-10-19 13:54:35 +0200

    add Task 2 [Print String using Array of Characters]

---
+++ CProgramming-Day4/Tasks/Task2_DisplayString/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main()
+{
+    char Name[10] , ch;
+    int i;
+
+    printf("Please Enter Your Name \n");
+
+
+    ///using gets and puts [Not Need to make delimeter]
+    //gets(Name);
+    //puts(Name);
+
+
+    /// Using getChe [Null terminator (delimeter \0) ]
+
+    for(i=0 ; i<10 ; i++){
+        ch = _getche();
+        if(ch == 13) {
+            Name[i] = '\0';
+            break;
+        }
+        Name[i] = ch;
+    }
+
+    printf("\n");
+
+    for(i=0 ; i<10 ; i++){
+        printf("%c" , Name[i]);
+        if(Name[i] == '\0') break;
+    }
+
+
+
+
+    return 0;
+}
