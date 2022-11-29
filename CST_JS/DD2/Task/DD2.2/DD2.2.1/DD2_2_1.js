1129fceffa15323b4c96fe80db1937b34c794b5a
zeynab335 <zeynab335>
2022-11-29 19:43:24 +0200

    finishing task 2 part 1 in Day2 in js

--- CST_JS/DD2/Task/DD2.2/DD2.2.1/DD2_2_1.js
+++ CST_JS/DD2/Task/DD2.2/DD2.2.1/DD2_2_1.js
@@ -1,39 +1,51 @@
-//? Task1 Point 1.1 in Day2
+//? Task2 Point 2.1 in Day2
 
-//& CountStringCharacters Funcitoon
-function CountStringCharacters(){
-    var stringFromUsr = prompt("Enter Your string");
-    var confirmDiffCharacter = confirm("Case Sensitive or not");
-    var Expression =  prompt("Enter Your Character");
-    
-    var strLen = 0;
-    ///* case inSesitive
-    if(!confirmDiffCharacter){
-        var regEx = new RegExp(Expression , 'i');
-        
-        for(var i in stringFromUsr){
-            if(stringFromUsr[i].match(regEx)){
-                strLen++;
-            }
-        }
-        
-        document.write("length = " + strLen);
-
-    }
-    else {
-        ///* case Sensitive
-        var regEx = new RegExp(Expression);
-        
-        for(var i in stringFromUsr){
-            if(stringFromUsr[i].match(regEx)){
-                strLen++;
-            }
-        }
-        
-        document.write("length = " + strLen);
-    }
+//&  Sum Funciton
+function Sum(num1 , num2 , num3) {
+    var sum = num1 + num2 + num3;
+    var sumEqusion = num1 + "+" + num2 + "+" + num3 + " = " + sum + "<br>"  ;
+    document.write(
+        "<p class='red'> Sum of the 3 Values =  </p> " + sumEqusion  + "<br>" 
+    )
 }
 
-CountStringCharacters();
+//&  Multiplication Funciton
+function Multiplication(num1 , num2 , num3) {
+    var multi = num1 * num2 * num3;
+    var multiEqusion = num1 + "*" + num2 + "*" + num3 + " = " + multi ;
+    document.write(
+        "<p class='red'> Multiplication of the 3 Values =  </p> " + multiEqusion + "<br>" 
+    )
+}
 
+//&  Division Funciton
+function Division(num1 , num2 , num3) {
+    var devision = num1 / num2 / num3;
+    var devisionEqusion = num1 + "/" + num2 + "/" + num3 + " = " + devision ;
+    document.write(
+        "<p class='red'> Division of the 3 Values =  </p> " + devisionEqusion  + "<br>"   
+    )
+}
+
+
+//&  GetElements Funciton
+function GetElements(){
+    //* Array of Elements
+    var Elements = [];
+    for(var i=0 ; i<3 ; i++){
+        Elements[i] = parseInt(prompt(`Enter Element => ${i+1}`));
+    }
+
+    //* invoke Sum Function
+    Sum(Elements[0],Elements[1],Elements[2]);
+    
+    //* invoke Multiplication Function
+    Multiplication(Elements[0],Elements[1],Elements[2]);
+    
+    //* invoke Division Function
+    //* can use Destructure Array
+    Division(...Elements);
+
+};
+GetElements()
 
