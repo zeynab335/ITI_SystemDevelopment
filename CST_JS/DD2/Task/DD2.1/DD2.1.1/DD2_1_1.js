94b2c921f98ac192845146541dcc9fc3e0e8566d
zeynab335 <zeynab335>
2022-11-29 18:58:08 +0200

    make code more readable in js in day 2

--- CST_JS/DD2/Task/DD2.1/DD2.1.1/DD2_1_1.js
+++ CST_JS/DD2/Task/DD2.1/DD2.1.1/DD2_1_1.js
@@ -1,37 +1,32 @@
 //? Task1 Point 1.1 in Day2
 
+function checkSenstivity(stringFromUsr,Sensitive,Expression){
+    var strLen = 0;
+    var regEx = Sensitive 
+    //* case inSesitive
+    ? new RegExp(Expression)
+    //* case inSesitive
+    : new RegExp(Expression , 'i')
+
+    for(var i in stringFromUsr){
+        if(stringFromUsr[i].match(regEx)){
+            strLen++;
+        }
+    }
+    
+    document.write("length = " + strLen);
+}
+
+
 //& CountStringCharacters Funcitoon
 function CountStringCharacters(){
     var stringFromUsr = prompt("Enter Your string");
-    var confirmDiffCharacter = confirm("Case Sensitive or not");
+    var Sensitive = confirm("Case Sensitive or not");
     var Expression =  prompt("Enter Your Character");
     
-    var strLen = 0;
-    ///* case inSesitive
-    if(!confirmDiffCharacter){
-        var regEx = new RegExp(Expression , 'i');
-        
-        for(var i in stringFromUsr){
-            if(stringFromUsr[i].match(regEx)){
-                strLen++;
-            }
-        }
-        
-        document.write("length = " + strLen);
+    //& invoke checkSenstivity function
+    checkSenstivity(stringFromUsr,Sensitive,Expression)
 
-    }
-    else {
-        ///* case Sensitive
-        var regEx = new RegExp(Expression);
-        
-        for(var i in stringFromUsr){
-            if(stringFromUsr[i].match(regEx)){
-                strLen++;
-            }
-        }
-        
-        document.write("length = " + strLen);
-    }
 }
 
 CountStringCharacters();
--- CST_JS/DD2/Task/DD2.1/DD2.1.2/DD2_1_2.js
+++ CST_JS/DD2/Task/DD2.1/DD2.1.2/DD2_1_2.js
@@ -1,5 +1,22 @@
 //? Task 1 Point 1.2 in Day2
 
+function checkPaliderome(strArr , caseSensitivity){
+    for(var i=0 ; i<strArr.length ; i++){
+        var str = strArr[i];
+        var ReverseArr = str.split("").reverse();
+        var strReverse  = ReverseArr.join("");
+        
+        var cond = caseSensitivity ? (str === strReverse) : (str.match('i') === strReverse.match('i'))
+        
+        if(cond){
+            document.write("Match ==> " +  str + " " + strReverse + "<br/>");
+        }   
+        else {
+            document.write("Not Match ==> " +  str +  " "+   strReverse + "<br/>");
+        }
+    }
+}
+
 //& palindromeFun Function
 function palindromeFun() {
     var originStr = prompt("palindrome ");
@@ -8,38 +25,7 @@ function palindromeFun() {
     var caseOption = confirm("Case Sensitive or not");
     
     //* Case SEnsitive
-    if(caseOption){
-        for(var i=0 ; i<strArr.length ; i++){
-            var str = strArr[i];
-            var ReverseArr = str.split("").reverse();
-            var strReverse  = ReverseArr.join("");
-    
-            if(str === strReverse){
-                document.write("Match ==> " +  str + " " + strReverse + "<br/>");
-            }   
-            else {
-                document.write("Not Match ==> " +  str +  " "+   strReverse + "<br/>");
-            }
-        }
-        
-    }
-    else {
-        //* Case inSEnsitive
-        for(var i=0 ; i<strArr.length ; i++){
-            var str = strArr[i];
-            var ReverseArr = str.split("").reverse();
-            var strReverse  = ReverseArr.join("");
-            
-            if(str.match('i') === strReverse.match('i')){
-                document.write("Match ==> " +  str + " " +  strReverse + "<br/>");
-            } 
-            else {
-                document.write("Not Match ==> " +  str +  " "+   strReverse + "<br/>");
-            }  
-            
-        }
-        
-    }
+    checkPaliderome(strArr , caseOption) ;
 
    
 }
