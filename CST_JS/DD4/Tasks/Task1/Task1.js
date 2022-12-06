5d9dcbb9ed4c93ad67d07ef7cd7814e420101907
zeynab335 <zeynab335>
2022-12-06 14:22:26 +0200

    finishing register page with formating

---
+++ CST_JS/DD4/Tasks/Task1/Task1.js
@@ -0,0 +1,18 @@
+/*
+    * 1. Location Object & Navigator Object
+    * A.1.1. Make your own welcoming page of a registration form to
+        & display a greeting message for the user by his name and title
+        & show a display of his info (address, gender, email, mobile)  
+        & display a recommendation to use chrome browser if he is using another browser (Bonus)
+
+
+    * You should build a registration form to send the entire 
+        * required info from the user and let the action is getting the welcoming page.
+        * The welcoming page is the page that should be displayed after registration.
+        * The registration form should contain fields for 
+            * name, email, password, job title, mobile, gender, and address.
+            * Read the required info from the query string
+*/
+
+
+
