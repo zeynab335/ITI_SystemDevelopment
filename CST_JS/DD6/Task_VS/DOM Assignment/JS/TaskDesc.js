861e52be6add8de0c7addc028e4be60f64845b7e
zeynab335 <zeynab335>
2022-12-09 11:41:29 +0200

    Finish Task2 in D6 in CST_JS

---
+++ CST_JS/DD6/Task_VS/DOM Assignment/JS/TaskDesc.js
@@ -0,0 +1,6 @@
+/*
+
+    *A.2. Given a web document as shown below in Fig. a, with only these few lines of code in shown in Fig
+    * b. Using DOM Nodes to access, create, modify or remove any of its elements to have it finally as shown in Fig. c.
+*/
+
