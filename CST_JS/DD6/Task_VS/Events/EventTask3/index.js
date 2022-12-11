11bdce576d8bd5a5ca38687ffbe2ad4bc70282cd
zeynab335 <zeynab335>
2022-12-11 11:46:34 +0200

    finishing last tsk in events in CST_jS

--- CST_JS/DD6/Task_VS/Events/EventTask3/index.js
+++ CST_JS/DD6/Task_VS/Events/EventTask3/index.js
@@ -1,13 +1,34 @@
-var btnSubmit = document.getElementById('btnSubmit');
+var btnSubmit = document.querySelector('.btnSubmit');
 
+//* check if user submitted or not
+var flag = 0;
 
+//* Submitted Form
+btnSubmit.addEventListener("click",function(e){
+    var confirm = window.confirm("Are You Want to Submit your Applicant? ");
+
+    if(!confirm){
+        e.preventDefault();
+        flag = 0;        
+    }
+    else{
+        flag = 1
+        var ConfirmationMsg = document.getElementById('ConfirmationMsg')
+        ConfirmationMsg.innerText = "Thanks For compeleted this Application Form 😊💕 ";
+        ConfirmationMsg.style.backgroundColor = '#52BD8F'
+    }
+    
+})
 
 //* custom event
 var timeOut = new Event('timeOut');
 
 document.body.addEventListener('timeOut',function(){
-    var TimeOut = document.getElementById('TimeOut');
-    TimeOut.innerText  = "Time Out 🔥 , Please Enter Your Data then Submit"
+    if(!flag){
+        var TimeOut = document.getElementById('TimeOut');
+        TimeOut.innerText  = "Time Out 🔥 , Please Enter Your Data then Submit"
+    }
+    
 })
 
 //* dispatchEvent => trigger the events
@@ -15,4 +36,4 @@ setTimeout(
     function(){
         document.body.dispatchEvent(timeOut);
     }
-,10000)
+,30000)
