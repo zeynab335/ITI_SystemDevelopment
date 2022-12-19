b185e2529bbe9e4f07628b1c32ac15c31f2300d4
zeynab335 <zeynab335>
2022-12-19 11:32:11 +0200

    add task 2.4 in Advanced Js course in Day1

---
+++ Advanced_Js/D1/Tasks/Task2/Task2.4/Task2_4.js
@@ -0,0 +1,12 @@
+var cookieName = "Cookie"
+var cookieValue = "value"
+
+
+setCookie (cookieName,cookieValue)
+//setCookie ()
+//getCookie ()
+console.log(getCookie (cookieName));
+//deleteCookie ()
+// allCookieList (cookieName)
+console.log(allCookieList ())
+hasCookie ()
