9558eab723acd43296add584288b81755d29b63d
zeynab335 <zeynab335>
2022-12-19 15:10:50 +0200

    add task 2.4 in Advanced js

--- Advanced_Js/D1/Tasks/Task2/Task2.1/Task2_2.js
+++ Advanced_Js/D1/Tasks/Task2/Task2.1/Task2_2.js
@@ -8,7 +8,7 @@ function getReverseFunctionV1(){
     Elements.reverse();
     document.getElementById('ReversedElements').innerText = "Reversed Elements = " +  Elements; 
 }
-
+/*
 function getReverseFunctionV2(){
         
     var Elements = Object.entries(arguments);
@@ -16,6 +16,25 @@ function getReverseFunctionV2(){
     for (let i = 0; i < Elements.length; i++) {
         Elements[i] = Elements[i][1]
     }
+
+    //* displat Element in sorted way
+    document.getElementById('ElementsV2').innerText = "Elements = " +  Elements;
+
+    Elements.reverse();
+    //* displat Element in reversed way
+    document.getElementById('ReversedElementsV2').innerText = "Reversed Elements = " +  Elements; 
+
+}
+*/
+
+
+function getReverseFunctionV2(){
+
+   //* when use bind => Element()
+   //* can use apply , call => without calling function
+   //* [Array.prototype.slice] This method can be used to convert the arguments object by binding this method to the object.
+   
+   var Elements = Array.prototype.slice.call(arguments);
 
     //* displat Element in sorted way
     document.getElementById('ElementsV2').innerText = "Elements = " +  Elements;
