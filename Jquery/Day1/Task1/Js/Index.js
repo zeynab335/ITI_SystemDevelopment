26a2ef25dd9bf86cd511a141df807f7f7f150b49
zeynab335 <zeynab335>
2022-12-21 20:53:02 +0200

    finishing slider of Menu Gallery

--- Jquery/Day1/Task1/Js/Index.js
+++ Jquery/Day1/Task1/Js/Index.js
@@ -7,25 +7,86 @@
 //* Toggle Active Class in Menu List
 $(function(){
     var Btns = $(".MenuListButtons button");
+    
     for (let i = 0; i < Btns.length; i++) {
+        //* to make first Menu List Active
+        if(Btns.eq(i).attr('class') =='active') {
+            $('.'+Btns[i].id+'Details').eq(0).css('display','flex')
+            $('.MenuListDetails').fadeIn(500);
+        }
 
         Btns.eq(i).click(function(){
             if(Btns.eq(i).attr('class')!=='active') {
-                                    //* Hide all items in menu list
+                //* Hide all items in menu list
 
                 for(var j=0 ; j<Btns.length ; j++)
                 {   
                     if(j!== i) {
                         Btns.eq(j).removeClass('active')
-                        $('.'+Btns[j].id+'Details').eq(0).css('display','none')
+                        $('.'+Btns[j].id+'Details').hide();
                     }
                 }
                 Btns.eq(i).attr('class','active')
                 //AboutDetails  ==> dispaly flex 
-                $('.'+Btns[i].id+'Details').eq(0).css('display','flex')
+                $('.'+Btns[i].id+'Details').eq(0).fadeIn(1000).css('display','flex');
             }
             
         })   
     }
    
 })
+
+
+
+//* Make Gallery Slider
+$(function(){
+
+    var arr = [
+        "1.jpg",
+        "2.jpg",
+        "3.jpg",
+        "4.jpg",
+        "5.jpg",
+        "6.jpg",
+        "7.jpg",
+        "8.jpg"
+    ]
+    $('#ArrowLeft').click(function(){
+        // 1.jpg
+        var ImgElement = $('.GalleryContainer img');
+        var Img = ImgElement.get(0).src ;
+        
+        var ActiveImg = Img.slice(Img.length-5 , Img.length);
+        var index = arr.indexOf(ActiveImg);
+        var ImgFullPath = '../Images/'
+
+        if(index === 0 ){   
+            ImgElement.attr('src' , ImgFullPath + arr[7]);
+        }
+        
+        else{
+            ImgElement.attr('src' , ImgFullPath + arr[index-1]);
+        }
+        
+    })
+
+    $('#ArrowRight').click(function(){
+        // 1.jpg
+        var ImgElement = $('.GalleryContainer img');
+        var Img = ImgElement.get(0).src ;
+        
+        var ActiveImg = Img.slice(Img.length-5 , Img.length);
+        var index = arr.indexOf(ActiveImg);
+        var ImgFullPath = '../Images/'
+
+        if(index === 7 ){   
+            ImgElement.attr('src' , ImgFullPath + arr[0]);
+        }
+        
+        else{
+            ImgElement.attr('src' , ImgFullPath + arr[index+1]);
+        }
+        
+    })
+})
+
