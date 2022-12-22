c2565e014b445b38c4915ab63dad9623151e97fc
zeynab335 <zeynab335>
2022-12-22 23:58:04 +0200

    add append Function

--- Advanced_Js/D3/Task1/Task.js
+++ Advanced_Js/D3/Task1/Task.js
@@ -1,45 +1,66 @@
-var obj = {
-    getSetGen : getSetGen
-} 
+//* TODO: Create your own object
+//*Your constructor takes 3 parameters to define start, end of list and step
 
-function getSetGen() {
-    //* ['Name', 'Age', 'getSetGen']
-    var properties = Object.keys(this);
-    var caller = this;
+function LinkedList(start , end , step){
 
-    //* for each prop => create setter , getter method
-    for (var i = 0; i < properties.length; i++) {
+    //The list should be private and filled with private method
+    var List = [];
+    //& check in Arrguments if NAN
+    if(CheckTypeOfArrguments(start , end , step))
+    {
+        //? check if lisy is empty or not
+        var isEmpty = function(){
+            if(List.length === 0 )return 1;
+            return 0;
+        }
 
-        var Setter = "set" + properties[i] ; //? setName
-        var Getter = "get" + properties[i] ; //? getName
+        //& getter and setter for the list 
+        this.getList  = function(){
+            console.log("%c Array Elements = " , "color:blue");
+            List.forEach(function(ele){
+                console.log(ele);
+            });
+            console.log("%c***********************" , "color:blue");
 
-        //* IIFE => Immediate Invoking Function Expression 
-        //* [to avoid Clusure problem with blocking scope ]
-        caller[Setter] = (function(i){
-            //? send parameter Value to set in Properties
-            return function(Value){
-                caller[properties[i]] = Value
+        }
+
+        // & Append or prepend a new value 
+        this.Append  = function(ele){
+            if(isEmpty && ele === start){
+                //* when List is Empty 
+                List.push(ele);
             }
-        })(i)
-
-        caller[Getter] = (function(i){
-            return function(){
-                return caller[properties[i]]
+                
+            else if(((List[List.length-1] + step) === ele) && ele <= end){ 
+                //* [2,4] and step = 2 
+                //? check if element increase from last element in list with step number 
+                List.push(ele);
             }
-        })(i)
+            else{
+                throw new Error("Invalid Number, Please Try Again");
+            }
+        
+        }
+        // & Dequeue or pop a value,
     }
-    
 }
 
-Employee = {Name:"emp" , Age:15 , Address:''};
-console.log(Employee)
 
-obj.getSetGen.call(Employee);
 
-Employee.setName("ddd")
-Employee.setAge(10)
+//& Check type of Arrguments
+function CheckTypeOfArrguments(start , end , step){
+    var isArrgumentsValid = isFinite(start) &&  isFinite(end) &&  isFinite(step)
+    if(!isArrgumentsValid) throw new Error("Please Enter Valid Parameters, it must be a number");
+    return 1;
+}
 
-console.log(Employee)
-console.log(Employee.getName())
+
+
+var l1 = new LinkedList(2,5,1);
+l1.Append(2)
+l1.Append(3)
+l1.Append(4)
+
+l1.getList()
 
 
--- Advanced_Js/D3/Task1/TaskDesc.js
+++ Advanced_Js/D3/Task1/TaskDesc.js
@@ -1,13 +1,21 @@
-/*
-    * .1. Create your own custom object that has getSetGen as a function value,
-    * this function should generate setters and getters 
-    * for the properties of the caller object
-    * This object may have a description property of string value if needed
-    Let any other created object can use this function property to 
-    generate getters and setters for its own properties
-    Avoid generating getters or setters for any property of function 
-    value
-
+/* 
+    * TODO: Create your own object that contains a list of numerical sequence, with the following details 
+        * Your constructor takes 3 parameters to define start, end of list and step
+        * The list should be private and filled with private method
+        & You can create getter and setter for the list if needed
+        * Allow the user to apply the following functionality to his created sequence
+            & Append or prepend a new value 
+            & Dequeue or pop a value,
+            ? you have to ensure that you are pushing sequential value 
+                ! otherwise through exception
+            ? you have to ensure that there is no duplicated value 
+                ! otherwise through exception
+        
+        *all of the properties should be defined using accessor and/or data descriptor,
+        * prevent them from being deleted, iterated or being modified.
+        
+        & Override .toString() function to display a message with all of the list content.
+        * you can add any property you need
 
 
 
