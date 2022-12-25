6ec6b07a9ff0f89876c780d6fc30c4fc440c3a0b
zeynab335 <zeynab335>
2022-12-25 23:45:07 +0200

    add task 1 in jquery in d2

--- Jquery/Day2/Task1/Scripts/Task.js
+++ Jquery/Day2/Task1/Scripts/Task.js
@@ -1,1 +1,18 @@
+
+$(function(){
+    var counter = 0;
+    var end  = 500;
+    var timer = setInterval(()=>{
+        if(end == counter){
+            clearInterval(timer);
+        }
+        else{
+            counter += 20;
+            $('#img').eq(0).css('left', counter)
+        }
+        $('#imgSrc').eq(0).text('<img src="12.gif" style="left:"'+ counter + 'px"/>')
+    },1000)
     
+
+
+})
