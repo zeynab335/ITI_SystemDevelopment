772e47adcbe32c2227f7be61183d8309effeff62
zeinab Mahmoud <71345608+zeynab335@users.noreply.github.com>
2023-02-02 00:53:46 +0200

    update D08 in C#

---
+++ C#/D09/D09/D09/Employee.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D09
+{
+    internal class Employee
+    {
+        // fields
+        int vacationStock;
+
+        // Event
+        public event EventHandler<EmployeeLayOffEventArgs> EmployeeLayOff;
+
+        // Invoke Event
+        protected virtual void OnEmployeeLayOff (EmployeeLayOffEventArgs e)
+        {
+            EmployeeLayOff?.Invoke(this, e);
+        }
+
+        public int EmployeeID { get; set; }
+        public DateTime BirthDate
+        {
+            get;
+            set;
+        }
+        public int VacationStock
+        {
+            get { return vacationStock; }
+            set {
+                if (value < 0)
+                {
+                    // fire event
+                    OnEmployeeLayOff(new() { Cause = LayOffCause.VacationStockLessThan0 });
+                }
+                else vacationStock = value;
+            }
+        }
+        public bool RequestVacation(DateTime From, DateTime To)
+        {
+            int numOfDays = (To.Day - From.Day);
+
+            if (vacationStock > 0 && numOfDays > vacationStock)
+            {
+                // fire event
+                OnEmployeeLayOff(new() { Cause = LayOffCause.VacationStockLessThan0 });
+                return false;
+            }
+            else
+            {
+                // calculate number of days of vacation
+                vacationStock -= numOfDays;
+                return true;
+            }
+        }
+        public void EndOfYearOperation()
+        {
+            if (DateTime.Now.Year - BirthDate.Year > 60)
+            {
+                // fire event
+                OnEmployeeLayOff(new() { Cause = LayOffCause.AgeGreaterThan60 });
+            }
+            
+        }
+    }
+}
---
+++ C#/D09/D09/D09/SalesPerson.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D09
+{
+    internal class SalesPerson : Employee
+    {
+        public int AchievedTarget { get; set; }
+
+        //  Sales Employee will be Fired if Failed to Achieve Sales Target
+        public bool CheckTarget(int Quota)
+        {
+            if(Quota < AchievedTarget )
+            {
+               OnEmployeeLayOff(new() { Cause = LayOffCause.NotAchievedTarget});
+                return false;
+            }
+            return true;
+
+        }
+
+        protected override void OnEmployeeLayOff(EmployeeLayOffEventArgs e)
+        {
+            if (e.Cause == LayOffCause.NotAchievedTarget)
+            {
+                base.OnEmployeeLayOff(e);
+            }
+        }
+    }
+}
