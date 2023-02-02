3506594d8d7e07e7e123961d62b031903a5affef
zeinab Mahmoud <71345608+zeynab335@users.noreply.github.com>
2023-02-02 22:54:06 +0200

    merge to main

--- C#/D09/D09/D09/Program.cs
+++ C#/D09/D09/D09/Program.cs
@@ -30,22 +30,23 @@
             s1.AchievedTarget = 200;
 
             d1.AddStaff(s1);
-            //
             c1.AddMember(s1);
             
+            // not fire event
             s1.EndOfYearOperation();
             s1.CheckTarget(100);
-
 
             BoardMember b1 = new BoardMember();
             b1.EmployeeID = 3;
             b1.VacationStock = 10;
             
             d1.AddStaff(b1);
-            //b1.BirthDate = new DateTime(1890, 2, 10);
-            //b1.RequestVacation(DateTime.Now, new DateTime(2023, 2, 15));
-
+            b1.BirthDate = new DateTime(1890, 2, 10);
+            // Not fire event
+            b1.RequestVacation(DateTime.Now, new DateTime(2023, 2, 15));
+            // Fire Event
             b1.Resign();
+
             
         }
 
