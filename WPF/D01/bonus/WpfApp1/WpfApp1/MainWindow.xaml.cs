4e5b2449db35429cf8b6713c3d0e75960c0d921f
zeinab Mahmoud <71345608+zeynab335@users.noreply.github.com>
2023-02-06 00:35:48 +0200

    Add files via upload

---
+++ WPF/D01/bonus/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void richbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            TextRange textRange = new TextRange(
+                // TextPointer to the start of content in the RichTextBox.
+                richbox.Document.ContentStart,
+                // TextPointer to the end of content in the RichTextBox.
+                richbox.Document.ContentEnd
+            );
+
+
+            FlowDocument myFlowDoc = new FlowDocument();
+
+            Paragraph myParagraph = new Paragraph();
+            myParagraph.Inlines.Add(textRange.Text.ToString().Trim() + " Append text");
+
+            // Add the paragraph to the FlowDocument.
+            myFlowDoc.Blocks.Add(myParagraph);
+            richbox.Document =  myFlowDoc;
+        }
+    }
+}
