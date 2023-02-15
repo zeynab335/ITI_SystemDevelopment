e7e32edd4df533dbf3fa21261e4c75c949676500
zeinab Mahmoud <71345608+zeynab335@users.noreply.github.com>
2023-02-15 04:38:03 +0200

    add Task 3 in D11 in C#

---
+++ C#/D11/Drag_Drop_Rectangle/Drag_Drop_Rectangle/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Drag_Drop_Rectangle
+{
+    public partial class Form1 : Form
+    {
+        Rectangle Rec = new Rectangle(0, 0, 100, 100);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            //Graphics grfs = this.CreateGraphics();
+            e.Graphics.FillRectangle(Brushes.SteelBlue, Rec);
+
+            base.OnPaint(e);
+
+        }
+
+       
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Rec.X = e.X ;
+                Rec.Y = e.Y ;
+                Invalidate();
+
+            }
+
+        }
+
+       
+    }
+}
--- C#/D11/Paint/Paint/Form1.cs
+++ C#/D11/Paint/Paint/Form1.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+//using System.Windows.Media;
 
 namespace Paint
 {
     public partial class Form1 : Form
     {
+        Color BrushColor { set; get; } = Color.White;
         public Form1()
         {
             InitializeComponent();
@@ -21,29 +23,32 @@ namespace Paint
         {
 
         }
-        protected override void OnPaint(PaintEventArgs e)
-        {
-            e.Graphics.DrawRectangle(Pens.Gray, 50, 50,100,100);
-            e.Graphics.FillRectangle(Brushes.Gray, 50, 50, 100, 100);
-
-            base.OnPaint(e);
-        }
-
+      
         private void MouselickHandler(object sender, MouseEventArgs e)
         {
             Graphics grfs = this.CreateGraphics();
-            
-            if(e.Button == MouseButtons.Left)
+            SolidBrush brush = new SolidBrush(BrushColor); 
+            var pen = new Pen(BrushColor);
+
+            if (e.Button == MouseButtons.Left)
             {
-                grfs.DrawRectangle(Pens.Gray, e.X-50, e.Y-50, 100, 100);
-                grfs.FillRectangle(Brushes.Gray, e.X - 50, e.Y-50, 100, 100);
+                grfs.DrawRectangle(pen, e.X - 50, e.Y - 50, 100, 100);
+                grfs.FillRectangle(brush, e.X - 50, e.Y - 50, 100, 100);
 
             }
 
             if (e.Button == MouseButtons.Right)
             {
-                grfs.FillRectangle(Brushes.White, e.X - 50, e.Y - 50, 110, 110);
+                grfs.DrawRectangle(Pens.SteelBlue, e.X - 50, e.Y - 50, 100, 100);
+                grfs.FillRectangle(Brushes.SteelBlue, e.X - 50, e.Y - 50, 100, 100);
+            }
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                BrushColor = colorDialog1.Color;
             }
         }
     }
