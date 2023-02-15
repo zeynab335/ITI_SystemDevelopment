74470899881027959be2f71dd3367df99e54eb23
zeinab Mahmoud <71345608+zeynab335@users.noreply.github.com>
2023-02-15 10:45:01 +0200

    add Task 4 in C#

---
+++ C#/D11/Mikey/Mikey/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Mikey
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exist = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // exist
+            // 
+            this.exist.BackColor = System.Drawing.Color.Black;
+            this.exist.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.exist.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exist.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.exist.Location = new System.Drawing.Point(660, 197);
+            this.exist.Name = "exist";
+            this.exist.Size = new System.Drawing.Size(48, 42);
+            this.exist.TabIndex = 0;
+            this.exist.Text = "X";
+            this.exist.UseVisualStyleBackColor = false;
+            this.exist.Click += new System.EventHandler(this.exist_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(80, 197);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(48, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 753);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.exist);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exist;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
