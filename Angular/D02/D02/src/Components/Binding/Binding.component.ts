08bf7e51678f791d35878ee25fcce41fc3bfefe1
zeinab Mahmoud <71345608+zeynab335@users.noreply.github.com>
2023-04-04 06:38:23 +0200

    add first task in D02 Angular

---
+++ Angular/D02/D02/src/Components/Binding/Binding.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Binding',
+  templateUrl: './Binding.component.html',
+  styleUrls: ['./Binding.component.css']
+})
+
+export class BindingComponent {
+
+  inputValue = "";
+
+
+
+
+
+}
